9b46022b0a4f73d949da6cf4583e2f3e5710a7e1
rodrigobeta <alanrod77@gmail.com>
2025-04-07 18:01:54 -0600

    variables en python

---
+++ variables.py
@@ -0,0 +1,38 @@
+
+# Variables en python
+
+# Variables numéricas (integers y floats)
+edad = 25
+altura = 1.75
+precio = 99.99
+
+# Variables de texto (strings)
+nombre = "Juan"
+apellido = 'Pérez'
+direccion = '''Calle Principal 
+              #123'''
+
+# Variables booleanas
+es_estudiante = True
+tiene_mascota = False
+
+# Variables de tipo lista
+colores = ['rojo', 'verde', 'azul']
+numeros = [1, 2, 3, 4, 5]
+
+# Variables de tipo tupla (inmutable)
+coordenadas = (10, 20)
+dias_semana = ('Lunes', 'Martes', 'Miércoles')
+
+# Variables de tipo diccionario
+persona = {
+    'nombre': 'Ana',
+    'edad': 30,
+    'ciudad': 'Madrid'
+}
+
+# Variables de tipo conjunto (set)
+frutas = {'manzana', 'naranja', 'plátano'}
+
+# Variable None (nulo)
+valor_nulo = None
